846d0e62c4970635f6eea79d1736f24de9bb0db7
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-21 16:37:23 -0700

    Added board tiles controller

---
+++ app/controllers/api/board_tiles_controller.rb
@@ -0,0 +1,64 @@
+class Api::BoardTilesController < ApplicationController
+  def index
+    @board_tiles = BoardTile.where(board_id: params[:board_id])
+    @board_tiles = @board_tiles.map { |board_tile| {
+      tile: Tile.find(board_tile[:tile_id]),
+      books: board_tile.player_tiles.all.map { |player_tile| Book.find(player_tile[:book_id]) },
+      recommendations: Tile.find(board_tile[:tile_id]).recommendations.all.map { |recommendation| { recommendation: recommendation, book: Book.find(recommendation[:book_id]) } }
+    } }
+
+    render :json => {
+      board_tiles: @board_tiles
+    }
+  end
+
+  def show
+    @board_tile = BoardTile.find(params[:id])
+
+    render :json => {
+      board_tile: @board_tile
+    }
+  end
+
+  def create
+    @board_tile = BoardTile.new(board_tile_params)
+
+    if @board_tile.save
+      render :json => {
+        board_tile: @board_tile
+      }
+    else
+      render :json => {
+        error: 'Board Tile was not saved'
+      }
+    end
+  end
+
+  def update
+    @board_tile = BoardTile.find(params[:id])
+
+    if @board_tile.update(board_tile_params)
+      render :json => {
+        board_tile: @board_tile
+      }
+    else
+      render :json => {
+        error: 'Board Tile was not updated'
+      }
+    end
+  end
+
+  def destroy
+    @board_tile = BoardTile.find(params[:id])
+    @board_tile.destroy
+
+    render :json => {
+      message: 'Board Tile was destroyed'
+    }
+  end
+
+  private
+    def board_tile_params
+      params.require(:board_tile).permit(:board_id, :tile_id)
+    end
+end
