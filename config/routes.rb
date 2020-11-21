aa957e02d3e3c5e2ed8fff50c8b66807383724f2
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-21 16:36:11 -0700

    Removed boilerplate test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do # /api/data
-
-    get '/data', to: 'tests#index'
+  namespace :api do
     
     resources :users 
     resources :players
