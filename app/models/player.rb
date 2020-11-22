75fba6a4f736f0e87eb0e68a1655df88faf1c11d
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-21 17:16:39 -0700

    Set player token as optional

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ApplicationRecord
   belongs_to :user
   belongs_to :game
   belongs_to :color
-  belongs_to :token
+  belongs_to :token, optional: true
 
   has_many :player_tiles
   has_many :player_badges
