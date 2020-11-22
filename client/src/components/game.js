5140d70fceabad2bbaf33a5cc527dd52d4b81fd6
William <williamzak0@gmail.com>
2020-11-22 01:56:43 +0000

    added side bar and responsive sizing

--- client/src/components/game.js
+++ client/src/components/game.js
@@ -4,6 +4,14 @@ import Board from "./gameBoard";
 
 export default function Game(props) {
   return (
-    <Board />
+    <body class="game-view">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <div class="side-bar">
+        Side Bar
+      </div>
+      <div class="game-play">
+        <Board />
+      </div>
+    </body>
   )
 }
