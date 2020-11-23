0c778d84e440c8fbdb1ebbbf5c6f4b9e677fca0a
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-23 16:57:33 -0700

    Fixed dice css

--- client/src/components/dice.js
+++ client/src/components/dice.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Dice from "react-dice-roll";
 
+import './dice.css'
+
 export default function Roll(props) {
   return(
-    <div class="dice">
+    <div class="dice"z>
       <Dice onRoll={(value) => console.log(value)} size={100}/>
     </div>
   )
