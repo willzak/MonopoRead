b763cb8548ba34ea4faa541d81ef337733b76f3f
William <williamzak0@gmail.com>
2020-11-23 23:04:49 +0000

    changed chance tile direction

--- client/src/components/chanceTile.js
+++ client/src/components/chanceTile.js
@@ -2,21 +2,26 @@ import React from "react";
 
 export default function Chance(props) {
   let type = "chance chance";
+  let text = "chance-text-";
 
   if (props.direction === 'left') {
     type += '-left';
+    text += 'left';
   } else if (props.direction === 'right') {
     type += '-right';
+    text += 'right';
   } else if (props.direction === 'top') {
     type += '-top';
+    text += 'top';
   } else {
     type += '-bottom'
+    text += 'bottom';
   }
 
   return (
     <div class={type}>
       {props.player}
-      <div class="chance-default"><strong>?</strong></div>
+      <div class={text}><strong>?</strong></div>
     </div>
   )
 }
