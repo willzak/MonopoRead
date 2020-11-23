b395fd359e90bf8f105681f66b5f693b2c856e11
William <williamzak0@gmail.com>
2020-11-23 23:37:01 +0000

    added text to chance tiles

--- client/src/components/chanceTile.js
+++ client/src/components/chanceTile.js
@@ -2,26 +2,35 @@ import React from "react";
 
 export default function Chance(props) {
   let type = "chance chance";
-  let text = "chance-text-";
+  let text = "chance-icon-";
+  let label = "label-";
 
   if (props.direction === 'left') {
     type += '-left';
     text += 'left';
+    label += 'left';
   } else if (props.direction === 'right') {
     type += '-right';
     text += 'right';
+    label += 'right';
   } else if (props.direction === 'top') {
     type += '-top';
     text += 'top';
+    label += 'top';
   } else {
-    type += '-bottom'
+    type += '-bottom';
     text += 'bottom';
+    label += 'bottom';
   }
 
   return (
     <div class={type}>
-      {props.player}
-      <div class={text}><strong>?</strong></div>
+      <div class={text}>
+        <strong>?</strong>
+      </div>
+      <div class={label}>
+        Chance
+      </div>
     </div>
   )
 }
--- client/src/components/square.js
+++ client/src/components/square.js
@@ -4,12 +4,15 @@ import { useRouteMatch } from "react-router-dom";
 
 export default function Square(props) {
   let type = "square " + props.direction + "-square";
+  let text = "square-text-" + props.direction;
 
   return (
     <div class={type}>
-       <Link to = {`/tiles/${props.id}`}>
-      <h4>{props.name}</h4>
-      </Link>
+      <span class={text}>
+        <Link to = {`/tiles/${props.id}`}>
+        <h4>{props.name}</h4>
+        </Link>
+      </span>
     </div>
   )
 }
