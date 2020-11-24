f4d4d34bbdaf84fcdce8fa0ef58f00219d1bb489
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-24 15:46:50 -0700

    Added call of drawChance if landed on chance card

--- client/src/components/chanceTile.js
+++ client/src/components/chanceTile.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Token from './token'
 
 export default function Chance(props) {
+
   let type = "tile chance chance";
   let text = "chance-icon-";
   let label = "label-";
@@ -24,9 +25,19 @@ export default function Chance(props) {
     label += 'bottom';
   }
 
+  useEffect(() => {
+    for (const player of props.players) {
+      if (props.pos === player.current_tile) {
+        if (player.done) props.drawChance(player.player.id)
+      }
+    }
+  }, [props.players])
+
   const activePlayers = function() {
     return props.players.map((player) => {
-      if (props.pos === player.current_tile) return <Token key={player.player.player.id} color={player.player.color.hexcode} />
+      if (props.pos === player.current_tile) {
+        return <Token key={player.player.player.id} color={player.player.color.hexcode} />
+      }
       else return null
     })
   }
