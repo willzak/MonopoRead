f771080217fad4cfa66cf4dafa1915c644e0d089
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-24 12:13:35 -0700

    Changed token background and used player color as border

--- client/src/components/token.js
+++ client/src/components/token.js
@@ -3,7 +3,7 @@ import "./token.css"
 
 export default function Token(props) {
   return (
-    <div className ="small-token">
+    <div className="small-token" style={{ borderColor: props.color }}>
     </div>
   )
 }
