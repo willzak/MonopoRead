b1ed766a80783e5f17c900f7ef79bf95ae536d29
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-24 13:42:06 -0700

    Pulled up api calls and added positions and dice roll

--- client/src/components/game.js
+++ client/src/components/game.js
@@ -7,6 +7,17 @@ import SideBar from "./sideBar";
 export default function Game(props) {
   const [game, setGame] = useState(0)
   const [board, setBoard] = useState(0)
+  const [players, setPlayers] = useState([])
+  const [positions, setPositions] = useState([])
+  const [tiles, setTiles] = useState([])
+
+  const rollDice = function(number, player) {
+    setPositions((current) => {
+      const newPositions = [...current]
+      newPositions[player] = {...newPositions[player], current_tile: ((newPositions[player].current_tile + number) % 24) }
+      return newPositions
+    })
+  }
 
   useEffect(() => {
     axios.get(`/api/games`)
@@ -32,16 +43,57 @@ export default function Game(props) {
     }
   }, [game])
 
+  useEffect(() => {
+    if (board !== 0) {
+      axios.get(`/api/boards/${board}/current_tiles`)
+      .then((response) => {
+        // handle success
+        setPlayers(response.data);
+      })
+    }
+  }, [board])
+
+  useEffect(() => {
+    if (board !== 0) {
+      axios.get(`/api/boards/${board}/board_tiles`)
+      .then((response) => {
+        // handle success
+        setTiles(response.data.map(tile => {
+          return {
+            tile: tile,
+            id: tile.tile.id,
+            name: tile.tile.name,
+            colour: tile.color.hexcode,
+            description: tile.tile.description,
+            recommendation: tile.recommendations.map(rec => rec.book.name)
+          }
+        }));
+      })
+    }
+  }, [board])
+
+  useEffect(() => {
+    const startPositions = []
+    for (const player in players) {
+      let p = { player: { ...players[player] } }
+      let current_tile = 0
+      if (players[player].current_tile) for (let i = 0; i < tiles.length; i++) if (tiles[i].id === current_tile.board_tile_id) current_tile = i
+      p.current_tile = current_tile
+      startPositions.push(p)
+    }
+    setPositions(startPositions)
+  }, [players, tiles])
+
   return (
     <section className="game-view">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <div className="side-console">
         <div className='title' >MONOPOREAD</div>
-        <SideBar board={board} />
+        <SideBar players={players} rollDice={rollDice} board={board} />
       </div>
       <div className="game-play">
         <Router>
-          <Board board={board} />
+          <Board tiles={tiles} players={positions} board={board} />
         </Router>
       </div>
     </section>
