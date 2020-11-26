d25c653e3889a0a690f6ead7bea56ef52ac03c38
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-25 19:17:19 -0700

    Fixed displaying multiple submitted books

--- client/src/components/tileDescription.js
+++ client/src/components/tileDescription.js
@@ -6,7 +6,7 @@ export default function Description( props ) {
     <h3> {props.description}</h3>
      
     <h2> {props.books.length > 0 ? "What others have read:" : ''}</h2>
-    <h3> {props.books}</h3>
+    {props.books.map((book, index) => <h3 key={index}>{book}</h3> )}
     <h2> We Recommend: </h2>
     <h3> {props.firstbookrec} </h3>
     <h3> {props.secondbookrec} </h3>
