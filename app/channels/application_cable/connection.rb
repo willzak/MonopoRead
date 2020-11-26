ae93e365799865d93f843f1d4c5ac56bb1925aed
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-26 14:06:29 -0700

    Added action cable to rails api

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        if current_user = User.find_by(id: cookies.signed[:user_id])
+          current_user
+        else
+          current_user = User.order(:id).first
+        end
+      end
   end
 end
