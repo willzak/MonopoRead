d604f45f2b519489cdce4ced6efcaf6c2875a4e3
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-26 14:44:02 -0700

    Attempt to fix websockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,5 @@ Rails.application.configure do
   
   config.action_cable.url = "ws://localhost:3000/cable"
   config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = %r{http://*}
 end
