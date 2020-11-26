8d7b5f03d6d167f3c1529d7696f68d9639127834
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-25 20:31:16 -0700

    Fixed bug with submitted books array

--- client/src/components/tileDescription.js
+++ client/src/components/tileDescription.js
@@ -5,7 +5,7 @@ export default function Description( props ) {
     <div>
     <h3> {props.description}</h3>
      
-    <h2> {props.books.length > 0 ? "What others have read:" : ''}</h2>
+    <h2> {props.books[0] && props.books.length > 0 ? "What others have read:" : ''}</h2>
     {props.books.map((book, index) => <h3 key={index}>{book}</h3> )}
     <h2> We Recommend: </h2>
     <h3> {props.firstbookrec} </h3>
