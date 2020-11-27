89b14a25c88a018325e2887c41ebe56c505173f9
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-11-27 17:15:38 +0000

    set up a basic hardcoded form for Player color selection

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,10 +1,18 @@
 import React from "react"
 
 
+
 export default function Player( props ) {
   return (
     <div>
       <h1> Player Form  </h1>
+      <label for="player-color">Choose a Color: </label>
+      <select name="colors">
+        <option value="Red">Red</option>
+        <option value="Pint">Pink</option>
+        <option value="Blue">Blue</option>
+        <option value="Yellow">Yellow</option>
+      </select>
     </div>
   )
 }
