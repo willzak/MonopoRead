d1023ba9b0b54ece27299f0c506c6e3b3a40519f
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-11-27 02:12:11 +0000

    Moved Router component to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from "./components/game"
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Game />, document.getElementById('root'));
+  <Router> 
+    <Game />
+  </Router>, 
+  document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
