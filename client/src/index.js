f696413fc0374705eb38568944d7f963f363e238
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-27 15:19:33 -0700

    Pulled application data creation to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from "./components/game"
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App.js"
