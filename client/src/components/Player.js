c522bbea852c05446ac2ab25b25d8d02c8d5b4f8
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-11-27 21:12:26 +0000

    got the color picker form working

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,18 +1,42 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import axios from 'axios';
 
 
 export default function Player( props ) {
+  // const [game, setGame] = useState(1)
+  const [colors, setColors] = useState([])
+  const [name, setName] = useState("")
+
+useEffect(() => {
+    axios.get(`/api/games/1`)
+      .then((response) => {
+        console.log("response for game data:", response.data.name)
+        setName(response.data.name)
+      })
+  }, [])
+
+  useEffect(() => {
+   axios.get(`api/games/1/free_colors`)
+      .then((response) => {
+        console.log("This is response data:", response.data)
+        setColors(response.data.map(color=> color.name))
+      })
+  }, [])
+  console.log("this is colors", colors)
+
   return (
     <div>
       <h1> Player Form  </h1>
+      <h2> Game Name: {name}</h2>
+      <form>
       <label for="player-color">Choose a Color: </label>
-      <select name="colors">
-        <option value="Red">Red</option>
-        <option value="Pint">Pink</option>
-        <option value="Blue">Blue</option>
-        <option value="Yellow">Yellow</option>
-      </select>
+        <select name="colors">
+          {colors.map ((color, index) =>{
+          return <option key={index} > {color}</option>
+          })}
+        </select>
+      </form>
+      <button> Submit </button>
     </div>
   )
 }
