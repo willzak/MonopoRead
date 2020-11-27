16fade415846c5d0cc330e65c946b36acc25fbc6
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-27 15:26:30 -0700

    Added play game button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import { Link } from "react-router-dom";
 
 export default function Home( props ) {
   return (
     <div>
       <h1> Home Page </h1>
+      <Link to = {`/board/`}>
+        <h1>Play Game</h1>
+      </Link> 
     </div>
   )
 }
