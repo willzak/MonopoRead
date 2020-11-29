8833e8d79f2329de93aed7cdebe924cabf647bf1
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-29 14:35:38 -0700

    Removed create game button if not logged in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -36,14 +36,16 @@ export default function Home(props) {
         {props.joinableGames.map((game, index) => {
           return <h3 onClick={() => props.setGame(game)} key={index}>{game.name}</h3>
         })}
-      </Link> 
-      <Link to={`/game`}>
-        <h2>Create Game</h2>
       </Link>
-        <h2>Change User</h2>
-        {props.users.map((user, index) => {
-          return <h3 style={{width: '100px', border: (props.user.id === user.id) ? '1px solid black' : 'none'}} onClick={() => props.login(user.email, 'password')} key={index}>{user.name}</h3>
-        })}
+      {props.user ? (
+        <Link to={`/game`}>
+          <h2>Create Game</h2>
+        </Link>
+      ) : null}
+      <h2>Change User</h2>
+      {props.users.map((user, index) => {
+        return <h3 style={{width: '100px', border: (props.user.id === user.id) ? '1px solid black' : 'none'}} onClick={() => props.login(user.email, 'password')} key={index}>{user.name}</h3>
+      })}
       <button onClick={props.logout}>Logout</button>
     </div>
   )
