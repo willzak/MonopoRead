4e286f81e96eb8c9ef2cc5ee14718255c98e372a
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-01 11:28:34 -0700

    Added cheat array for dice for presentation

--- client/src/components/dice.js
+++ client/src/components/dice.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Dice from "react-dice-roll";
 
 import './dice.css'
 
 export default function Roll(props) {
+  const [index, setIndex] = useState(0)
+  const cheat = [3, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
+
+  const roll = function(value, currentPlayer) {
+    props.rollDice(value, currentPlayer)
+    setIndex(current => current + 1)
+  }
   return(
     <div className="dice">
-      <Dice disabled={props.disabled} onRoll={(value) => props.rollDice(value, props.currentPlayer)} size={100} />
+      <Dice disabled={props.disabled} onRoll={(value) => roll(value, props.currentPlayer)} size={100} cheatValue={cheat[index] % cheat.length} />
     </div>
   )
 }
