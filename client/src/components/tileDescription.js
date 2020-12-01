2b07293e185a9eaf90c979e605a83c8092a21428
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-12-01 19:46:09 +0000

    tile description all centered

--- client/src/components/tileDescription.js
+++ client/src/components/tileDescription.js
@@ -25,8 +25,9 @@ export default function Description( props ) {
         <h3 className="outcome">+3 Points</h3>
       </div>
       </div>) :
-    (<div>
-    <h3> {props.description}</h3>
+    (<div className="outer-body">
+    <h3 className="book-description"> {props.description}</h3>
+    
     <div className="submitted-books">
       <h2 style={{marginBottom: '0'}}> {props.books[0] ? "What others have read:" : ''}</h2>
       {props.books[0] && props.books.length > 5 ? (
@@ -40,12 +41,15 @@ export default function Description( props ) {
         </List>
       ) : (props.books.map((book, index) => <h3 key={index} onMouseEnter={() => onMouseEnter(book.review)} onMouseLeave={() => props.setShowReview(false)}>{book.name}</h3>))}
     </div>
+    
+    <div>
     <h2> We Recommend: </h2>
       <ul className ="book-rec-list" >
       <li className="indv-book-recs">{props.firstbookrec} </li>
       <li className="indv-book-recs">{props.secondbookrec} </li>
       <li className="indv-book-recs">{props.thirdbookrec} </li>
       </ul>
-    </div>)
+    </div>
+  </div>)
   )
 }
