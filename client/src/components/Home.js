758c6cb80335d65b234a108c1f6d01ec5ceccbc8
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-01 17:38:44 -0700

    Updated dice and seeds for presentation and testing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -45,13 +45,6 @@ export default function Home(props) {
         </div>
       </div>
 
-      <h3>Log in as:</h3>
-      <div className="users">
-        {props.users.map((user, index) => {
-          return <h3 style={{textAlign: 'center', padding: '10px', width: '100px', border: (props.user.id === user.id) ? '1px solid black' : 'none'}} onClick={() => props.login(user.email, 'password')} key={index}>{user.name}</h3>
-        })}
-      </div>
-
       <div className="game-options">
 
         {props.user ? (
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,10 +1,5 @@
 describe("Navigation", () => {
   it("should login", () => {
-    let position = 0;
-    const squares = [1, 2, 4, 5, 7, 8, 10, 11, 13, 14, 16, 17, 19, 20, 22, 23];
-    const chance = [3, 9, 15, 21];
-    const go = [0, 12];
-    const train = [6, 18];
     let bookPosition = 0;
     const books = ["Title"]
     let reviewPosition = 0;
@@ -12,18 +7,20 @@ describe("Navigation", () => {
 
     cy.visit("/");
 
-    cy.get(".users").contains("Avvai").click();
+    cy.get(".users").contains("User2").click();
     cy.get(".community-chest").click();
     cy.get('input[name=title]').type('New Game')
     cy.contains('Submit').click();
+    cy.get(".small-token").click({ force: true });
+    cy.wait(4000)
 
-    var genArr = Array.from({length:250},(v,k)=>k+1)
+    var genArr = Array.from({length:30},(v,k)=>k+1)
     cy.wrap(genArr).each(() => {
-      cy.get(".dice").click().then(() => console.log(position));
-      cy.wait(3000).then(() => {
-        position = (position + 5) % 24
-        if (squares.includes(position)) {
-          cy.get(".small-token").click({ force: true });
+      cy.get(".dice").click();
+      cy.wait(4000)
+      cy.get(".small-token").click({ force: true });
+      cy.url().then((response) => {
+        if (response.includes('board/tiles')) {
           cy.contains('Completed').click();
           cy.get('input[name=title]').type(books[bookPosition % books.length]);
           bookPosition++;
@@ -34,15 +31,9 @@ describe("Navigation", () => {
           cy.wait(4000)
           cy.get('.MuiSvgIcon-root').click();
         }
-        // else if (chance.includes(position)) {
-          
-        // }
-        // else if (go.includes(position)) {
-          
-        // }
-        // else if (train.includes(position)) {
-  
-        // }
+        else {
+          cy.wait(4000)
+        }
       })
     })
   });
